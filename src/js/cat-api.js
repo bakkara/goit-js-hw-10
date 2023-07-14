6ae1bd92da04d4370e90abcb7a3f353c0a41ab42
bakkara <ane4karymar@gmail.com>
2023-07-14 23:55:49 +0300

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// import SlimSelect from 'slim-select'
 import { elements, BASE_URL, END_POINT, breedSelect } from "./refs";
 
 function fetchBreeds() {
@@ -38,37 +37,3 @@ function createMarkup(arr) {
 }
 
 export {fetchBreeds, fetchCatByBreed, createMarkup}
-
-// function fetchBreeds() {
-    
-//     return axios.get(`${BASE_URL}${END_POINT}`)
-//         .then(response => {
-//             if (response.status !== 200) {
-//                 throw new Error(response.statusText);
-//             }
-//             /* breedSelect.style.display = "block"; */
-//             elements.loader.style.display = "none";
-//             return response.data;
-//         });
-// }
-
-// function fetchCatByBreed(breedId) {
-//     elements.loader.style.display = "block";
-//     return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-//         .then(response => {
-//             if (response.status !== 200) {
-//                 elements.error.style.display = "block";
-//                 throw new Error(response.statusText);
-//             }
-//             elements.loader.style.display = "none";
-//             return response.data;
-//         });
-// }
-
-// function createMarkup(arr) {
-//     const { url, breeds } = arr[0];
-//     return `<img src="${url}" alt="${breeds[0].name}" width="300">
-//     <h2 class="title-cat">${breeds[0].name}</h2>
-//     <p class="description-cat">${breeds[0].description}</p>
-//     <p class="temperament-cat">Temperament: ${breeds[0].temperament}</p>`;
-// }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,6 @@
 const API_KEY = "live_HqCR5PyR8vjKZX5ffwJ1lhg7zIzMV3JZebeYJdXgeImkpO0WkkWfQUFcV8I3fqAk";
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT = '/breeds';
-// import SlimSelect from 'slim-select';
-// import 'slim-select/dist/slimselect.css'
  
 const elements = {
     select: document.querySelector('#single'),
@@ -10,6 +8,5 @@ const elements = {
     loader: document.querySelector(".loader"),
     error: document.querySelector(".error"),
 }
-// const breedSelect = new SlimSelect({ select: '#single' });
 
 export {API_KEY, elements, BASE_URL, END_POINT, breedSelect}
